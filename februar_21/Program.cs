ca8d907db9a75e88ee47317235659490f4753269
Marci02 <nagymarci.0218@gmail.com>
2023-02-21 19:46:53 +0100

    +1 parameter

--- februar_21/Program.cs
+++ februar_21/Program.cs
@@ -13,74 +13,132 @@ namespace februar_21
 
             Console.SetWindowSize(80, 25);
             Console.SetBufferSize(80, 25);
-            Console.ForegroundColor = ConsoleColor.Gray;    
+            Console.ForegroundColor = ConsoleColor.Gray;
 
-            Negyszog(10,8,20,10);
+            Console.Write($"Dupla vagy egy vonalas keretet szeretnél: ");
+            string adat = Console.ReadLine();
+
+            Negyszog(10,8,20,10, adat);
 
             Console.ReadKey();
         }
 
-        private static void Negyszog(int left,int top, int width, int heigth)
+        private static void Negyszog(int left,int top, int width, int heigth, string adat)
         {
-            //Bal felső
-
-            Console.SetCursorPosition(left, top);
-            Console.Write('╔');
-
-            //Jobb felső
-            
-            Console.SetCursorPosition(left + width, top);
-            Console.Write('╗');
-            
-            //Bal alsó
-            
-            Console.SetCursorPosition(left, top + heigth);
-            Console.Write('╚');
-            
-            //Jobb alsó
-
-            Console.SetCursorPosition(left + width, top + heigth);
-            Console.Write('╝');
-
-            //Vízszintes
-
-            for (int i = left + 1; i < left+width; i++)
+            if (adat == "dupla")
             {
-                Console.SetCursorPosition(i, top);
-                Console.Write('═');
-                Console.SetCursorPosition(i, top + heigth);
-                Console.Write('═');
-            } 
+
+                //Bal felső
+
+                Console.SetCursorPosition(left, top);
+                Console.Write('╔');
+
+                //Jobb felső
+            
+                Console.SetCursorPosition(left + width, top);
+                Console.Write('╗');
+            
+                //Bal alsó
+            
+                Console.SetCursorPosition(left, top + heigth);
+                Console.Write('╚');
+            
+                //Jobb alsó
+
+                Console.SetCursorPosition(left + width, top + heigth);
+                Console.Write('╝');
+
+                //Vízszintes
+
+                for (int i = left + 1; i < left+width; i++)
+                {
+                    Console.SetCursorPosition(i, top);
+                    Console.Write('═');
+                    Console.SetCursorPosition(i, top + heigth);
+                    Console.Write('═');
+                } 
             
            
             
-            //Függőleges
+                //Függőleges
 
-            for (int i = top + 1; i < top + heigth; i++)
-            {
-                Console.SetCursorPosition(left, i);
-                Console.Write('║');
-                Console.SetCursorPosition(left + width, i);
-                Console.Write('║');
-            }
-
-            for (int i = top + 1 ; i < top+heigth; i++)
-            {
-                for (int j = left + 1 ; j < left + width; j++)
+                for (int i = top + 1; i < top + heigth; i++)
                 {
-                    Console.SetCursorPosition(j,i);
-                    Console.Write('▓');
+                    Console.SetCursorPosition(left, i);
+                    Console.Write('║');
+                    Console.SetCursorPosition(left + width, i);
+                    Console.Write('║');
                 }
+
+                for (int i = top + 1 ; i < top+heigth; i++)
+                {
+                    for (int j = left + 1 ; j < left + width; j++)
+                    {
+                        Console.SetCursorPosition(j,i);
+                        Console.Write('▓');
+                    }
                
-            }
-
-            for (int i = top - 2; i < top -2; i++)
-            {
-                for (int j = left + 2; j < left - 3; j++)
-                {
-                    Console.SetCursorPosition(j, i);
-                    Console.Write('▓');
                 }
+
+               
+
+            }
+            else
+            {
+
+                //Bal felső
+
+                Console.SetCursorPosition(left, top);
+                Console.Write('┌');
+
+                //Jobb felső
+
+                Console.SetCursorPosition(left + width, top);
+                Console.Write('┐');
+
+                //Bal alsó
+
+                Console.SetCursorPosition(left, top + heigth);
+                Console.Write('└');
+
+                //Jobb alsó
+
+                Console.SetCursorPosition(left + width, top + heigth);
+                Console.Write('┘');
+
+                //Vízszintes
+
+                for (int i = left + 1; i < left + width; i++)
+                {
+                    Console.SetCursorPosition(i, top);
+                    Console.Write('─');
+                    Console.SetCursorPosition(i, top + heigth);
+                    Console.Write('─');
+                }
+
+
+
+                //Függőleges
+
+                for (int i = top + 1; i < top + heigth; i++)
+                {
+                    Console.SetCursorPosition(left, i);
+                    Console.Write('│');
+                    Console.SetCursorPosition(left + width, i);
+                    Console.Write('│');
+                }
+
+                for (int i = top + 1; i < top + heigth; i++)
+                {
+                    for (int j = left + 1; j < left + width; j++)
+                    {
+                        Console.SetCursorPosition(j, i);
+                        Console.Write('▓');
+                    }
+
+                }
+
+               
 
             }
 
